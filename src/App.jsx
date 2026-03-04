9fd19234e8c0be818dd81d4c47d47bb9bb7ff347
Nazrul Islam <dobon.islam@icloud.com>
2026-03-03 21:29:20 -0500

    project setup and navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navbar from './shared/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar></Navbar>
+     <h1 className='text-3xl text-center'>This is a App page</h1>
+    </>
+  )
+}
+
+export default App
